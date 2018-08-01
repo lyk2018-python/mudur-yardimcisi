cffcf4696e1f2f21cd3b61d0277ef5d41956747b
beyef3ndi <realveli24@gmail.com>
2018-08-01 18:55:18 +0300

    Update : Token_Generator

--- muduryardimcisi/muduryardimci/views.py
+++ muduryardimcisi/muduryardimci/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django_otp.oath import hotp
-
+from .models import Courses,Profile
 import random
 def generate_token(request):
     key = bytes(random.randint(1000000,99999999))
@@ -8,4 +8,8 @@ def generate_token(request):
         token = hotp(key=key, counter=i, digits=6)
         if len(str(token)) < 6:
             token = (6 - len(str(token))) * str(random.randint(1, 9)) + str(token)
+    get_course_id = Profile.objects.get(user=request.user).course_id
+    get_course_token = Courses.objects.get(course_name=get_course_id)
+    get_course_token.course_token = token
+
     return render(request, 'auth.html', {"token": token})
