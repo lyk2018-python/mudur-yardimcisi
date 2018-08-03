87b2e656adb5dac1de6aec4d9320013d0f659b74
beyef3ndi <realveli24@gmail.com>
2018-08-03 15:20:14 +0300

    Update Module : Stundent_check

--- muduryardimcisi/muduryardimci/views.py
+++ muduryardimcisi/muduryardimci/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django_otp.oath import hotp
 from .models import Courses,Profile,Check,Site
 import random
-from time import gmtime, strftime
+from time import localtime, strftime
 
 def generate_token(request):
     key = bytes(random.randint(1000000,99999999))
@@ -29,6 +29,8 @@ def stundent_check(request):
             find_time = int(start_hour) + get_total_morning_date + get_total_afternoon_date
         elif time == "afternoon":
             find_time = int(start_hour) + get_total_morning_date
+        elif time == "morning":
+            find_time = float(start_hour)
         number_dec = int(str(find_time - int(find_time))[2:])
         number_dec = (number_dec / 10)
         find_hour = find_time - number_dec
@@ -37,15 +39,28 @@ def stundent_check(request):
         if find_start_min >= 60.0:
             find_start_min -= 60
             find_hour += 1
-        return(find_hour,find_start_min)
-
-    print(str(calucate_time("afternoon")))
-    hour_now, min_now = str(strftime("%H:%M", gmtime())).split(":")
-    #get_course_id = Profile.objects.get(user=request.user, is_trainer=False).course_id)
-    #get_check_id = Check.objects.get_or_create(course_id = get_course_id,user_id=request.user,course_check="")
-    #get_check_id.save()
+        start_min = float(find_start_min) / 100.0
+        find_time = float(find_hour) + start_min
+        return(find_time)
+    afternoon = calucate_time("afternoon")
+    evening = calucate_time("evening")
+    morning = calucate_time("morning")
+    print(morning,afternoon,evening)
+    hour_now, min_now = str(strftime("%H:%M", localtime())).split(":")
+    min_now = float(min_now) /100
+    hour_now = float(hour_now)
+    time_now = hour_now + min_now
+    if time_now > morning and time_now < afternoon and time_now < evening:
+        check_time = "morning"
+    elif time_now > morning and time_now > afternoon and time_now < evening:
+        check_time = "afternoon"
+    elif time_now > morning and time_now > afternoon and time_now > evening:
+        check_time = "evening"
+    print(check_time)
+    #get_course_id = Profile.objects.get(user=request.user, is_trainer=False).course_id
+    #get_or_create_check_id = Check.objects.get_or_create(course_id = get_course_id,user_id=request.user)
+    #get_or_create_check_id.save()
     return render(request, 'check_stundent.html',)
-
 def dashboard(request):
     check = Check.objects.all().values()
     return render(request, 'accounts/dashboard.html',{"check": check})
