60bb654f20955c7b3ced57a45f656cc12a8c60b1
beyef3ndi <realveli24@gmail.com>
2018-08-09 21:33:40 +0300

    Add : AuthTokenForm Add : AddNoteForm Update (Views) : stundent_check (Add Timeout And Fix Bugs) Update (Urls) : Add Paths

--- muduryardimcisi/muduryardimcisi/urls.py
+++ muduryardimcisi/muduryardimcisi/urls.py
@@ -8,11 +8,14 @@ urlpatterns = [
     path('login/', auth_views.login, name='login'),
     path('logout/', auth_views.logout, name='logout'),
     path('admin/', admin.site.urls),
-    path('auth/',  views.generate_token, name="auth"),
-    path('accounts/', views.dashboard, name='dashboard'),
+    path('auth/',  muduryardimci_views.generate_token, name="auth"),
+    path('accounts/', muduryardimci_views.dashboard, name='dashboard'),
     path('auth/',  muduryardimci_views.generate_token),
     path('', account_views.home),
     path('profile/', account_views.profile),
     path('accounts/', muduryardimci_views.dashboard, name='dashboard'),
     path('checkstundent/', muduryardimci_views.stundent_check, name="Stundent_check"),
+    path('authlogin/', muduryardimci_views.AuthToken, name="Authlogin"),
+    path('sucsessauth/', muduryardimci_views.SucssesAuth, name="SucsessAuth"),
+    path('addnote/', muduryardimci_views.AddNote, name="AddNote")
 ]
